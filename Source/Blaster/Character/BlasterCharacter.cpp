f62c2fcac4d6114bb2ec3d83373e0523486de2c8
Osman Fatih Çakır <osman@otyazilim.com>
2024-07-22 17:13:14 +0300

    blaster character

---
+++ Source/Blaster/Character/BlasterCharacter.cpp
@@ -0,0 +1,98 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlasterCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
+
+ABlasterCharacter::ABlasterCharacter()
+{
+  PrimaryActorTick.bCanEverTick = true;
+
+  CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+  CameraBoom->SetupAttachment(GetMesh());
+  CameraBoom->TargetArmLength = 600.f;
+  CameraBoom->bUsePawnControlRotation = true;
+
+  FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+  FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+  FollowCamera->bUsePawnControlRotation = false;
+}
+
+void ABlasterCharacter::BeginPlay()
+{
+  Super::BeginPlay();
+
+  //Add Input Mapping Context
+  if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+  {
+    if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+    {
+      Subsystem->AddMappingContext(DefaultMappingContext, 0);
+    }
+  }
+}
+
+void ABlasterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+  Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+  if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+  {
+    // Jumping
+    EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ThisClass::Jump);
+
+    // Moving
+    EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::Move);
+
+    // Looking
+    EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ThisClass::Look);
+  }
+}
+
+void ABlasterCharacter::Move(const FInputActionValue& Value)
+{  // input is a Vector2D
+  FVector2D movementVector = Value.Get<FVector2D>();
+
+  if (Controller != nullptr)
+  {
+    // find out which way is forward
+    const FRotator Rotation = Controller->GetControlRotation();
+    const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+    // get forward vector
+    const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+
+    // get right vector
+    const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+    // add movement
+    AddMovementInput(ForwardDirection, movementVector.Y);
+    AddMovementInput(RightDirection, movementVector.X);
+  }
+}
+
+void ABlasterCharacter::Look(const FInputActionValue& Value)
+{
+  // input is a Vector2D
+  FVector2D lookAxisVector = Value.Get<FVector2D>();
+
+  if (Controller != nullptr)
+  {
+    AddControllerYawInput(lookAxisVector.X);
+    AddControllerPitchInput(lookAxisVector.Y);
+  }
+}
+
+void ABlasterCharacter::Jump(const FInputActionValue& Value)
+{
+}
+
+void ABlasterCharacter::Tick(float DeltaTime)
+{
+  Super::Tick(DeltaTime);
+
+}
---
+++ Source/Blaster/Character/BlasterCharacter.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BlasterCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
+UCLASS()
+class BLASTER_API ABlasterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABlasterCharacter();
+
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	void Jump(const FInputActionValue& Value);
+
+protected:
+
+	// Input actions
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* JumpAction = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* MoveAction = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* LookAction = nullptr;
+
+	//
+
+	UPROPERTY(VisibleAnywhere, Category=Camera)
+	USpringArmComponent* CameraBoom = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	UCameraComponent* FollowCamera = nullptr;
+};
