50bb7fb582b501cd0bd1f6b59480eb16d40c48bd
Osman Fatih Çakır <osman@otyazilim.com>
2024-07-22 18:38:00 +0300

    game level

---
+++ Source/Blaster/GameMode/LobbyGameMode.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LobbyGameMode.h"
+#include "GameFramework/GameStateBase.h"
+
+void ALobbyGameMode::PostLogin(APlayerController* newplayer)
+{
+  Super::PostLogin(newplayer);
+
+  int32 numOfPlayers = GameState.Get()->PlayerArray.Num();
+  if (numOfPlayers == 1)
+  {
+    if (UWorld* world = GetWorld())
+    {
+      bUseSeamlessTravel = true;
+      world->ServerTravel(FString("/Game/Maps/BlasterMap?listen"));
+    }
+  }
+}
